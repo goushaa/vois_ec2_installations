51cb1fffdad8d0ec85586a1ba54de7da2ea1d00a
Ahmed ELKady <93336002+goushaa@users.noreply.github.com>
2024-08-22 15:32:17 +0300

    Update kadyVM1.sh

--- kadyVM1.sh
+++ kadyVM1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker run -d --name kady-jenkins -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 --restart=on-failure jenkins/jenkins:lts-jdk17
+docker exec -it kady-jenkins cat /var/jenkins_home/secrets/initialAdminPassword
